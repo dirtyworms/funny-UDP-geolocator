77729829af96feed22fb209ddb1a770932c998e6
dirtyworms <dirtcrawler69@gmail.com>
2026-05-03 01:50:35 -0500

    Add files via upload

---
+++ funny-UDP-geolocator.py
@@ -0,0 +1,84 @@
+import geoip2.database
+import socket
+import subprocess
+import ipaddress
+
+cmd = r"C:\Program Files\Wireshark\tshark.exe -i 5"
+
+process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+my_ip = socket.gethostbyname(socket.gethostname())
+
+reader = geoip2.database.Reader("GeoLite2-City.mmdb")
+asn_reader = geoip2.database.Reader("GeoLite2-ASN.mmdb")
+
+BLOCKED_ORGS = [
+    "Google", "Cloudflare", "Microsoft",
+    "Amazon", "Akamai", "Apple"
+]
+
+def is_bot_infra(ip):
+    try:
+        asn = asn_reader.asn(ip)
+        org = (asn.autonomous_system_organization or "").lower()
+
+        return any(bad.lower() in org for bad in BLOCKED_ORGS)
+
+    except:
+        return False
+
+
+def is_noise_ip(ip):
+    try:
+        ip_obj = ipaddress.ip_address(ip)
+        return ip_obj.is_private or ip_obj.is_loopback or ip_obj.is_multicast
+    except:
+        return True
+
+
+def get_ip_location(ip):
+    try:
+        response = reader.city(ip)
+
+        country = response.country.name or "Unknown"
+        subdivision = response.subdivisions.most_specific.name or "Unknown"
+        city = response.city.name or "Unknown"
+
+        return country, subdivision, city
+
+    except:
+        return "Unknown", "Unknown", "Unknown"
+
+
+for line in iter(process.stdout.readline, b""):
+    columns = str(line).split(" ")
+
+    if "UDP" in columns or "TCP" in columns or "SKYPE" in columns:
+
+        if "->" in columns:
+            src_ip = columns[columns.index("->") - 1]
+        elif "\\xe2\\x86\\x92" in columns:
+            src_ip = columns[columns.index("\\xe2\\x86\\x92") - 1]
+        else:
+            continue
+
+        if src_ip == my_ip:
+            continue
+
+        # 🔥 FILTERS GO HERE (this was broken before)
+        if is_noise_ip(src_ip):
+            continue
+
+        if is_bot_infra(src_ip):
+            continue
+
+        try:
+            country, sub, city = get_ip_location(src_ip)
+            print(f">>> {country}, {sub}, {city}")
+        except:
+            try:
+                real_ip = socket.gethostbyname(src_ip)
+                country, sub, city = get_ip_location(real_ip)
+                print(f">>> {country}, {sub}, {city}")
+            except:
+                print("Not found")
